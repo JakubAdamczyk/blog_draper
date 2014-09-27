3cf2096830301f13bcb537168ec522de0ff1de95
Jakub Adamczyk <andrzej@golonka.pl>
2014-09-27 10:32:02 +0200

    add article decorator

---
+++ app/decorators/article_decorator.rb
@@ -0,0 +1,24 @@
+class ArticleDecorator < Draper::Decorator
+  delegate_all
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+  def publication_status(article) 
+    if published? 
+     "Published at #{published_at}" 
+   else 
+     "Unpublished" 
+   end 
+
+   def published_at 
+    object.updated_at.strftime("%A, %B %e") 
+  end
+
+end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,11 +1,11 @@
 module ArticlesHelper
 
   def publication_status(article) 
- if article.published? 
- "Published at #{article.updated_at.strftime('%A, %B %e')}" 
- else 
- "Unpublished" 
- end 
-end
+    if article.published? 
+     "Published at #{article.updated_at.strftime('%A, %B %e')}" 
+   else 
+     "Unpublished" 
+   end 
+ end
 
 end
